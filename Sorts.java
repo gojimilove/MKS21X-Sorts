b438425be7727e99cb3c0c8a033f5d504f50d3f2
Jessica Kim <jkim00@stuy.edu>
2018-12-04 20:48:14 -0500

    mostly working, weird bug i cant figure out

---
+++ Sorts.java
@@ -0,0 +1,34 @@
+public class Sorts {
+	public static void printArray(int[]ary) {
+		System.out.print("[");
+		for (int i = 0; i < ary.length; i++) {
+			System.out.print(ary[i]);
+			if (i < ary.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.print("]\n\n");
+	}
+
+	public static void selectionsort(int[] ary) {
+		int minIndex = 0; //initialize index of min value 
+		for (int i = 0; i < ary.length; i++) {
+			//find min value from i to the end
+			for (int j = i; j < ary.length; j++) {
+				if (ary[j] <= ary[minIndex]) {
+					minIndex = j;
+					
+				}
+				System.out.println(""+minIndex);
+			}
+			//swap that index and index i
+			System.out.println("\n"+ary[i]);
+			System.out.println(""+ary[minIndex]);
+			printArray(ary);
+			int swapped = ary[i];
+			ary[i] = ary[minIndex];
+			ary[minIndex] = swapped;
+			printArray(ary);
+		}
+	}
+}
