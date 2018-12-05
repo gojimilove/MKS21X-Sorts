589df458dd8006b81a79b16388ebfb25413308e0
Jessica Kim <jkim00@stuy.edu>
2018-12-04 21:10:38 -0500

    fixed bug!

--- Sorts.java
+++ Sorts.java
@@ -13,23 +13,20 @@ public class Sorts {
 	public static void selectionsort(int[] ary) {
 		int minIndex = 0; //initialize index of min value 
 		for (int i = 0; i < ary.length; i++) {
+			//reset minIndex
+			minIndex = i;
+
 			//find min value from i to the end
 			for (int j = i; j < ary.length; j++) {
 				if (ary[j] <= ary[minIndex]) {
 					minIndex = j;
-					
 				}
 			}
-			System.out.println("Index: "+minIndex);
-			System.out.println("Value: "+ary[minIndex]);
+
 			//swap that index and index i
-			System.out.println("\n"+ary[i]);
-			System.out.println(""+ary[minIndex]);
-			printArray(ary);
 			int swapped = ary[i];
 			ary[i] = ary[minIndex];
 			ary[minIndex] = swapped;
-			printArray(ary);
 		}
 	}
 }
