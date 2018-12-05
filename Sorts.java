952619411d337445a73eaf3466d5e35fbb675d79
Jessica Kim <jkim00@stuy.edu>
2018-12-04 21:11:26 -0500

    deleted some debugging stuff

--- Sorts.java
+++ Sorts.java
@@ -1,15 +1,4 @@
 public class Sorts {
-	public static void printArray(int[]ary) {
-		System.out.print("[");
-		for (int i = 0; i < ary.length; i++) {
-			System.out.print(ary[i]);
-			if (i < ary.length - 1) {
-				System.out.print(", ");
-			}
-		}
-		System.out.print("]\n\n");
-	}
-
 	public static void selectionsort(int[] ary) {
 		int minIndex = 0; //initialize index of min value 
 		for (int i = 0; i < ary.length; i++) {
