a8fc094e14ff7e913a98d9c9b3f90299b7a0d938
Jessica Kim <jkim00@stuy.edu>
2018-12-07 10:04:22 -0500

    finished bubbleSort

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,6 @@
 public class Sorts {
-	public static void selectionsort(int[] ary) {
-		int minIndex = 0; //initialize index of min value 
+	public static void selectionSort(int[] ary) {
+		int minIndex = 0; //initialize index of min value
 		for (int i = 0; i < ary.length; i++) {
 			//reset minIndex
 			minIndex = i;
@@ -16,6 +16,20 @@ public class Sorts {
 			int swapped = ary[i];
 			ary[i] = ary[minIndex];
 			ary[minIndex] = swapped;
+		}
+	}
+
+	public static void bubbleSort(int[] data){
+		for (int i = 0; i < data.length - 1; i++) {
+			//System.out.println("RUN"+(i+1));
+			for (int j = 0; j < data.length - (i + 1); j++) {
+				int swapped = data[j];
+				if (data[j] > data[j + 1]) {
+					data[j] = data[j + 1];
+					data[j + 1] = swapped;
+				}
+				//System.out.println(""+data[j]+", "+data[j+1]);
+			}
 		}
 	}
 }
