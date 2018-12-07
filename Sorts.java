2cafe316e155916e2ce8f72a08c7acc91dbfb2aa
Jessica Kim <jkim00@stuy.edu>
2018-12-07 10:07:57 -0500

    minor documentation

--- Sorts.java
+++ Sorts.java
@@ -1,17 +1,18 @@
 public class Sorts {
+	/**Selection sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
 	public static void selectionSort(int[] ary) {
 		int minIndex = 0; //initialize index of min value
 		for (int i = 0; i < ary.length; i++) {
-			//reset minIndex
-			minIndex = i;
-
+			minIndex = i; //reset minIndex
 			//find min value from i to the end
 			for (int j = i; j < ary.length; j++) {
 				if (ary[j] <= ary[minIndex]) {
 					minIndex = j;
 				}
 			}
-
 			//swap that index and index i
 			int swapped = ary[i];
 			ary[i] = ary[minIndex];
@@ -19,11 +20,16 @@ public class Sorts {
 		}
 	}
 
+	/**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
 	public static void bubbleSort(int[] data){
-		for (int i = 0; i < data.length - 1; i++) {
+		for (int i = 0; i < data.length - 1; i++) { //(n-1)) runs
 			//System.out.println("RUN"+(i+1));
 			for (int j = 0; j < data.length - (i + 1); j++) {
 				int swapped = data[j];
+				//swap pair of numbers if first one is bigger
 				if (data[j] > data[j + 1]) {
 					data[j] = data[j + 1];
 					data[j + 1] = swapped;
