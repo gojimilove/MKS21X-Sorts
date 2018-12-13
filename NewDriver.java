867826dc31b08bc0bdfe3d3f12ca7e45effa7b43
Jessica Kim <jkim00@stuy.edu>
2018-12-12 22:08:26 -0500

    i still havent figured out what the problem was but its fixed now and i guess the problem was in driver

--- NewDriver.java
+++ NewDriver.java
@@ -5,8 +5,8 @@ public class NewDriver{
 
     int[] randish = new int[Integer.parseInt(artie[0])];
     for(int i = 0 ; i < randish.length; i++){
-      randish[i] = 0;
-      //randish[i] =(int)(Math.random()*10000);
+      //randish[i] = 0;
+      randish[i] =(int)(Math.random()*10000);
     }
 
     if(artie[1].equals("selection")){
